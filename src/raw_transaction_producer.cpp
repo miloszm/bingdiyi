fa7d08a87466f521296369e4b5b2c86c6dc54be5
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-03-03 22:41:18 +0100

    improved commenst

--- src/raw_transaction_producer.cpp
+++ src/raw_transaction_producer.cpp
@@ -21,33 +21,56 @@ void construct_raw_transaction(
     cout << "public hex: " << pubKey << endl;
     cout << "private hex: " << privKey << endl;
 
-    // make output
+    /**
+     * make output
+     * payment_address decodes base58 address and calculates hash for it
+     * to_pay_key_hash_pattern then creates the script, filling out the address
+     */
     script outputScript = script().to_pay_key_hash_pattern(payment_address(targetAddr).hash());
     output output1(satoshisToTransfer, outputScript);
 
-    // utxo
+    /**
+     * make utxo
+     * funding transaction id is decoded from hex string into bytes and reversed
+     * hash_digest is simply an array of 32 bytes
+     */
     string hashString = srcTxId;
     hash_digest utxoHash;
     decode_hash(utxoHash, hashString);
     output_point utxo(utxoHash, srcTxOutputIndex);
 
-    // previous locking script
+    /**
+     * previous locking script
+     * convert public key to chunk data, and then calculate sha 256 and ripemd160 on it
+     * then it is being placed in a locking script created from scratch
+     */
     data_chunk pubKeyChunk;
     pubKey.to_data(pubKeyChunk);
     script lockingScript = script().to_pay_key_hash_pattern(bitcoin_short_hash(pubKeyChunk));
     std::cout << "\nPrevious Locking Script: " << lockingScript.to_string(0xffffffff) << std::endl;
 
-    // make Input
+    /**
+     * make input
+     * input consists of previous output, which is utxo
+     */
     input input1 = input();
     input1.set_previous_output(utxo);
     input1.set_sequence(0xffffffff);
 
-    // build TX
+    /**
+     * build TX
+     * we build transaction with inputs and outputs
+     * input is not endorsed at this moment yet
+     */
     transaction tx = transaction();
     tx.inputs().push_back(input1);
     tx.outputs().push_back(output1);
 
-    // endorse TX
+    /**
+     * build endorsement
+     * endorsement is created out of the private key, locking script, transaction, input index
+     * endorsement is really a hashed signature of data given in
+     */
     endorsement sig;
     if(lockingScript.create_endorsement(sig, privKeyEC.secret(), lockingScript, tx, 0u, all))
     {
@@ -55,18 +78,23 @@ void construct_raw_transaction(
         std::cout << encode_base16(sig) << "\n" << std::endl;
     }
 
-    // make Sig Script
+    /**
+     * make Sig Script
+     * unlocking script is created from scratch here as a list of operations
+     */
     operation::list sigScript;
     sigScript.push_back(operation(sig));
     sigScript.push_back(operation(pubKeyChunk));
     script unlockingScript(sigScript);
     std::cout << "\nUnlocking Script: " << unlockingScript.to_string(0xffffffff) << std::endl;
 
-    // make Signed TX
+    /**
+     * make Signed TX
+     * fill out input with unlocking script which was missing until this point
+     */
     tx.inputs()[0].set_script(unlockingScript);
     std::cout << "Raw Transaction: " << std::endl;
     std::cout << encode_base16(tx.to_data()) << std::endl;
-
 }
 
 //private key WIF: cT6gppgsgc84CFxL7mtZHNncHjysHvFrsNoCXa2PHFqfECX2xAeX
