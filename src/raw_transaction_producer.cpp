34ca2a40ab032a190b4cecd617e0dc28d2df5060
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-03-03 21:43:07 +0100

    successfully created transaction which worked

--- src/raw_transaction_producer.cpp
+++ src/raw_transaction_producer.cpp
@@ -8,27 +8,30 @@ using namespace bc::chain;
 using namespace bc::wallet;
 using namespace bc::machine;
 
+
+
+//private key WIF: cT6gppgsgc84CFxL7mtZHNncHjysHvFrsNoCXa2PHFqfECX2xAeX
+//        funding tx id: eeeefc8137dc5c1254f578b027d446a22a119a4473da8867d181a4a232404511
+//        funding index: 1
+//target address: mihBbdmqPut61bs9eDYZ3fdYxAKEP3mdiX
+//        amount: 81000
+
+
 int main() {
     // private key for source_addr
-    const string privKeyWIF {"cN9XS1bFNhMmmvXTNudnwZd7zyuRwCk4HmEVy4xbSxtPArC4KcoE"};
-    const wallet::ec_private privKeyEC(privKeyWIF);
-    const wallet::ec_public pubKey = privKeyEC.to_public();
-    const libbitcoin::config::base16 privKey = libbitcoin::config::base16(privKeyEC.secret());
-    // amount to transfer in Satoshis
-    const uint64_t satoshisToTransfer {80000};
-    // fee in Satoshis - as calculated with help of bx fetch-utxo, where we can see
-    // if srcAddr has sufficient funds for the main transfer, the change, and the fee
-    const uint64_t fee {10000};
-    // source address
-    const string srcAddr {""};
+    const string privKeyWIF {"cT6gppgsgc84CFxL7mtZHNncHjysHvFrsNoCXa2PHFqfECX2xAeX"};
     // source transaction id (as found out via bx fetch-tx)
-    const string srcTxId {"dd91b944e8b743b1cdb8d106161e8d1f1a666597cc16a6291f7fd4baca891508"};
+    const string srcTxId {"eeeefc8137dc5c1254f578b027d446a22a119a4473da8867d181a4a232404511"};
     // source transaction's output index (as found out via bx fetch-tx)
     const int srcTxOutputIndex {1};
     // main target address
-    const string targetAddr {"n2JZCSr8MeGuGtvRVjZTqgNNw9pyYW98Pm"};
-    // target address for a change
-    // const string targetRemainderAddr {""};
+    const string targetAddr {"mihBbdmqPut61bs9eDYZ3fdYxAKEP3mdiX"};
+    // amount to transfer in Satoshis
+    const uint64_t satoshisToTransfer {81000};
+
+    const wallet::ec_private privKeyEC(privKeyWIF);
+    const wallet::ec_public pubKey = privKeyEC.to_public();
+    const libbitcoin::config::base16 privKey = libbitcoin::config::base16(privKeyEC.secret());
 
 
     cout << "priv WIF: " << privKeyEC << endl;
@@ -51,17 +54,17 @@ int main() {
     script lockingScript = script().to_pay_key_hash_pattern(bitcoin_short_hash(pubKeyChunk));
     std::cout << "\nPrevious Locking Script: " << lockingScript.to_string(0xffffffff) << std::endl;
 
-    //make Input
+    // make Input
     input input1 = input();
     input1.set_previous_output(utxo);
     input1.set_sequence(0xffffffff);
 
-    //build TX
+    // build TX
     transaction tx = transaction();
     tx.inputs().push_back(input1);
     tx.outputs().push_back(output1);
 
-    //Endorse TX
+    // endorse TX
     endorsement sig;
     if(lockingScript.create_endorsement(sig, privKeyEC.secret(), lockingScript, tx, 0u, all))
     {
@@ -69,14 +72,14 @@ int main() {
         std::cout << encode_base16(sig) << "\n" << std::endl;
     }
 
-    //make Sig Script
+    // make Sig Script
     operation::list sigScript;
     sigScript.push_back(operation(sig));
     sigScript.push_back(operation(pubKeyChunk));
     script unlockingScript(sigScript);
     std::cout << "\nUnlocking Script: " << unlockingScript.to_string(0xffffffff) << std::endl;
 
-    //Make Signed TX
+    // make Signed TX
     tx.inputs()[0].set_script(unlockingScript);
     std::cout << "Raw Transaction: " << std::endl;
     std::cout << encode_base16(tx.to_data()) << std::endl;
