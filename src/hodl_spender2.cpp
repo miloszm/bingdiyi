a1376bef1ba1cc0f8915ae07f1cf534cf88f0092
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-03-09 14:11:50 +0100

    WIP new edition of spender2

---
+++ src/hodl_spender2.cpp
@@ -0,0 +1,98 @@
+#include <bitcoin/bitcoin.hpp>
+
+using namespace std;
+using namespace bc;
+using namespace bc::chain;
+using namespace bc::wallet;
+using namespace bc::machine;
+
+
+operation::list to_pay_key_hash_pattern_with_delay(const data_chunk& publicKey, const uint32_t lockUntil)
+{
+    vector<uint8_t> lockUntilArray(4);
+    serializer<vector<uint8_t>::iterator>(lockUntilArray.begin()).write_4_bytes_little_endian(lockUntil);
+
+    return operation::list
+            {
+                    { lockUntilArray },
+                    { opcode::checklocktimeverify },
+                    { opcode::drop },
+                    { publicKey },
+                    { opcode::checksig }
+            };
+}
+
+void construct_raw_transaction(
+        const string privKeyWIF,
+        const string srcTxId,
+        const int srcTxOutputIndex,
+        const uint32_t srcLockUntil,
+        const string targetAddr,
+        const uint64_t satoshisToTransfer
+){
+    const wallet::ec_private privKeyEC(privKeyWIF);
+    const wallet::ec_public pubKey = privKeyEC.to_public();
+    const libbitcoin::config::base16 privKey = libbitcoin::config::base16(privKeyEC.secret());
+    data_chunk pubKeyChunk;
+    pubKey.to_data(pubKeyChunk);
+
+    cout << "priv WIF: " << privKeyEC << endl;
+    cout << "public hex: " << pubKey << endl;
+    cout << "private hex: " << privKey << endl;
+
+    // bx tx-encode -i 2a0990b736e79e1d65ce3e9e25427e36855235829d58c1f2a9eac18142c926a6:0:0 -o n4eaAFB3GPmrJR4ummYpQmYTx2VaNftuPe:800000 -l 1615161540
+
+    string hashString = srcTxId;
+    hash_digest utxoHash;
+    decode_hash(utxoHash, hashString);
+    output_point utxo(utxoHash, srcTxOutputIndex);
+    input input1 = input();
+    input1.set_previous_output(utxo);
+    input1.set_sequence(0);
+
+    script currentLockingScript = script().to_pay_key_hash_pattern(payment_address(targetAddr).hash());
+    output output1(satoshisToTransfer, currentLockingScript);
+
+    transaction tx = transaction();
+    tx.inputs().push_back(input1);
+    tx.outputs().push_back(output1);
+    tx.set_locktime(srcLockUntil);
+
+
+    // bx input-sign 156bc5c6f50244b6745e7280671697e8eb703a81a7daf1180ec65d957bff429e "[c4684560] checklocktimeverify drop [0314488ebfec9889c4253fe2d1a21715b932864d2892193e4ca60e0acbd1d9fd1d] checksig" 0100000001a626c94281c1eaa9f2c1589d82355285367e42259e3ece651d9ee736b790092a0000000000000000000100350c00000000001976a914fdbbbe6062fef2fca812e404e3dcb43dcdb4108888acc4684560
+
+    script redeemScript = to_pay_key_hash_pattern_with_delay(pubKeyChunk, srcLockUntil);
+    if(redeemScript.is_valid())
+    {
+        std::cout << "CLTV Script is Valid!" << std::endl;
+    }else{
+        std::cout << "CLTV Script Invalid!" << std::endl;
+    }
+    endorsement sig;
+    if(script::create_endorsement(sig, privKeyEC.secret(), redeemScript, tx, 0u, all))
+    {
+        std::cout << "Signature: " << encode_base16(sig) << std::endl;
+    }
+
+    // bx input-set "zero [3044022024048cd26f0d493173c4c1e15be7fc4bb0c9f91bbba422d46d09b910ec28c0ac02202477ce166b20b13aee4e997c5e2ffbdd8d274d06066307690f75dd7dcc5a3a6a01] [04c4684560b175210314488ebfec9889c4253fe2d1a21715b932864d2892193e4ca60e0acbd1d9fd1dac]" 0100000001a626c94281c1eaa9f2c1589d82355285367e42259e3ece651d9ee736b790092a0000000000000000000100350c00000000001976a914fdbbbe6062fef2fca812e404e3dcb43dcdb4108888acc4684560
+
+    operation::list sigScript;
+    sigScript.push_back(operation(sig));
+    sigScript.push_back(redeemScript.to_data(0));
+    script scriptUnlockingPreviousLockingScript(sigScript);
+    tx.inputs()[0].set_script(scriptUnlockingPreviousLockingScript);
+
+    std::cout << "Raw Transaction: " << std::endl;
+    std::cout << encode_base16(tx.to_data()) << std::endl;
+}
+
+int main() {
+    const string privKeyWIF {"cW4s2jFq7LZAJ8sz3Mwadq79j9ATabyeA6uLyhe8wjVisGDnxXRB"};
+    const string srcTxId {"a8491ce93f80959cff648930f32de759c451ad4b116f29b058bf7ee54129eb55"};
+    const int srcTxOutputIndex {0};
+    const uint64_t satoshisToTransfer {300000};
+    const uint32_t srcLockUntil = 1615294800;
+    const string targetAddr {"mroQ8Hpydtv5p4mpF7Tbtjqkwi7wHXYZuR"};
+
+    construct_raw_transaction(privKeyWIF, srcTxId, srcTxOutputIndex, srcLockUntil, targetAddr, satoshisToTransfer);
+}
