514139bcd470487a4794c618b07670845ededff8
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-03-30 00:15:25 +0200

    first usage of own library

--- test/utility/address_converter.cpp
+++ test/utility/address_converter.cpp
@@ -1,9 +1,14 @@
 #define BOOST_TEST_MODULE bing_test
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(first_test)
+#include "utility/address_converter.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+BOOST_AUTO_TEST_CASE(address_conversion_test)
 {
-    int i = 2;
-    BOOST_TEST(i);
-    BOOST_TEST(i == 2);
+    string spk_hex = AddressConverter::base58_to_spk_hex("mpS14bFCZiHFRxfNNbnPT2FScJBrm96iLE");
+    BOOST_TEST(spk_hex == "76a91461c95cddadf465cac9b0751edad16624d01572c088ac");
 }
